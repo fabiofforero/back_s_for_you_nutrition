a1211a44d345d88f8c1163391373d54178fd1908
fabioforero <josforero@uniboyaca.edu.co>
2024-10-30 22:08:37 -0500

    base de datos actualizada

--- app/Http/Middleware/ExampleMiddleware.php
+++ app/Http/Middleware/ExampleMiddleware.php
@@ -7,7 +7,7 @@ namespace App\Http\Middleware;
 use Closure;
 
 
-class CorsMiddleware
+class ExampleMiddleware
 {
     /**
      * Handle an incoming request.
@@ -18,27 +18,6 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $headers = [
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, code-app'
-        ];
-
-
-        if ($request->isMethod('OPTIONS')) {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        }
-
-
-        $response = $next($request);
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
-
-
-        return $response;
+        return $next($request);
     }
 }
-
